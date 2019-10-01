feb2aef1befeecd0269a147541573aa9302ff7e3
Mark Zuber <mzuber88@gmail.com>
2019-09-30 20:50:02 -0400

    Initial parser and test class

---
+++ Query311Java/src/main/java/Parser311.java
@@ -0,0 +1,15 @@
+public class Parser311 {
+    private String pathTo311Json;
+    public Parser311(String pathTo311Json) {
+        this.pathTo311Json = pathTo311Json;
+    }
+
+    public void generate311HashTable() {
+
+    }
+
+    public boolean isValid311Json()
+    {
+        return false;
+    }
+}
---
+++ Query311Java/src/test/java/Parser311Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Parser311Test {
+
+    @Test
+    public void generate311HashTable() {
+    }
+
+    @Test
+    public void isValid311Json() {
+    }
+}
