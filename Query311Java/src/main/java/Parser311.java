cbb6132cb7e5ccec222b8632e21140def016f503
Mark Zuber <mzuber88@gmail.com>
2019-10-02 21:38:38 -0400

    Added ability to generate a hashmap from the 311 dgaph json data

--- Query311Java/src/main/java/Parser311.java
+++ Query311Java/src/main/java/Parser311.java
@@ -1,15 +1,114 @@
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Parser311 {
     private String pathTo311Json;
     public Parser311(String pathTo311Json) {
         this.pathTo311Json = pathTo311Json;
     }
 
-    public void generate311HashTable() {
+    private void process311Object(JsonReader jsonReader, HashMap<String, HashMap<String, Integer>> hmap) throws Exception
+    {
+        jsonReader.beginObject();
 
+        while (jsonReader.hasNext() && !JsonToken.END_OBJECT.equals(jsonReader.peek())) {
+            String name = jsonReader.nextName();
+
+            if (!hmap.containsKey(name)) {
+                HashMap<String, Integer> fieldMap = new HashMap<>();
+                if (JsonToken.STRING.equals(jsonReader.peek())) {
+                    String value = jsonReader.nextString();
+                    fieldMap.put(value, 1);
+                    hmap.put(name, fieldMap);
+                } else {
+                    jsonReader.skipValue();
+                }
+            } else {
+                HashMap<String, Integer> fieldMap = hmap.get(name);
+                if (JsonToken.STRING.equals(jsonReader.peek())) {
+                    String value = jsonReader.nextString();
+                    if (fieldMap.containsKey(value)) {
+                        Integer count = fieldMap.get(value);
+                        fieldMap.put(value, count + 1);
+                    } else {
+                        fieldMap.put(value, 1);
+                    }
+                } else {
+                    jsonReader.skipValue();
+                }
+
+            }
+        }
+
+        jsonReader.endObject();
     }
 
-    public boolean isValid311Json()
+    public HashMap<String, HashMap<String, Integer>> generate311HashTable() throws Exception {
+        if(!isValid311DgraphJson())
+            throw new Exception("Invalid Dgraph Json");
+
+        HashMap<String, HashMap<String, Integer>> hmap = new HashMap<>();
+        Gson gson = new Gson();
+        BufferedReader br;
+        br = new BufferedReader(new FileReader(this.pathTo311Json));
+        JsonReader jsonReader = gson.newJsonReader(br);
+
+        //go past set
+        jsonReader.beginObject();
+        jsonReader.nextName();
+        jsonReader.beginArray();
+
+        while(jsonReader.hasNext() && JsonToken.BEGIN_OBJECT.equals(jsonReader.peek())) {
+            process311Object(jsonReader,hmap);
+        }
+
+        return hmap;
+    }
+
+    private boolean rootHasSetArray(JsonReader jsonReader)
     {
-        return false;
+        try {
+            jsonReader.beginObject();
+            String name = jsonReader.nextName();
+            if(!name.equals("set")) return false;
+            if(!JsonToken.BEGIN_ARRAY.equals(jsonReader.peek())) return false;
+            jsonReader.skipValue();
+            jsonReader.endObject();
+            JsonToken nextToken = jsonReader.peek();
+            if(!JsonToken.END_DOCUMENT.equals(jsonReader.peek())) return false;
+            System.out.println(nextToken);
+
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isValid311DgraphJson() {
+        Gson gson = new Gson();
+        BufferedReader br;
+
+        try {
+            br = new BufferedReader(
+                    new FileReader(this.pathTo311Json));
+        } catch (FileNotFoundException e) {
+            return false;
+        }
+
+        JsonReader jsonReader = gson.newJsonReader(br);
+
+        //dgraph root key must be set which is an array
+        if(!rootHasSetArray(jsonReader))
+            return false;
+
+        return true;
     }
 }
--- Query311Java/src/test/java/Parser311Test.java
+++ Query311Java/src/test/java/Parser311Test.java
@@ -9,6 +9,51 @@ public class Parser311Test {
     }
 
     @Test
-    public void isValid311Json() {
+    public void isValid311JsonBadPathIsFalse() {
+        Parser311 parser311 = new Parser311("/bad/path");
+
+        assertFalse(parser311.isValid311DgraphJson());
+    }
+
+    @Test
+    public void isValid311JsonGoodFile() {
+        Parser311 parser311 = new Parser311("test_data/valid_dgraph.json");
+
+        assertTrue(parser311.isValid311DgraphJson());
+    }
+
+    @Test
+    public void isValid311JsonHashMap() {
+        Parser311 parser311 = new Parser311("test_data/valid_dgraph.json");
+
+        try {
+            parser311.generate311HashTable();
+        }
+        catch (Exception e)
+        {
+            fail();
+        }
+
+        assertTrue(true);
+    }
+
+    @Test
+    public void invalidDgraphfile() {
+        Parser311 parser311 = new Parser311("test_data/invalid_dgraph.json");
+
+        assertFalse(parser311.isValid311DgraphJson());
+    }
+
+    @Test
+    public void invalidDgraphHashMap() {
+        Parser311 parser311 = new Parser311("test_data/invalid_dgraph.json");
+
+        try {
+            parser311.generate311HashTable();
+            fail();
+        }
+        catch (Exception e) {
+            assertTrue(true);
+        }
     }
 }
